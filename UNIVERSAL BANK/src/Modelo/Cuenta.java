a92141e954aa8cc211957e4e33b6ef5d10876567
Crimson <onassisalvarez37@gmail.com>
2025-07-26 17:17:51 -0500

    Ajuste
    -Se completó la clase Transaccion
    -Se ajusto la clase Cuenta

--- UNIVERSAL BANK/src/Modelo/Cuenta.java
+++ UNIVERSAL BANK/src/Modelo/Cuenta.java
@@ -1,16 +1,38 @@
 package Modelo;
 
 public class Cuenta {
-    String numerodecuenta; //Deberá generar un número al azar al crear una cuenta
-    TipoCuenta cuenta;
-    double saldo;
-    String fechadecreacion; //esto probablemente lo quite a la mrd
-    Boolean activa; //busca la cuenta aciva me ahorro detalles porque queda largo el texto
+    private String numerodecuenta;
+    private TipoCuenta tipo;
+    private double saldo;
+    private Boolean activa; //busca la cuenta aciva me ahorro detalles porque queda largo el texto
 
 
-    enum TipoCuenta {
+    public enum TipoCuenta {
         AHORRO,
         CORRIENTE
-        //No se cual es la otra
+    }
+
+    public Cuenta(TipoCuenta tipo,  double saldoInicial) {
+        this.numerodecuenta = generarNumeroUnico();
+        this.tipo = tipo;
+        this.saldo = saldoInicial;
+        this.activa = false;
+    }
+
+    private String generarNumeroUnico() {
+        return "C" + System.currentTimeMillis(); //vamos ya sabemos que hace no voy a explicar denuevo .-.
+    }
+
+    public String getNumerodecuenta() {return numerodecuenta;}
+    public TipoCuenta getTipo() {return tipo;}
+    public double getSaldo() {return saldo;}
+    public Boolean getActiva() {return activa;}
+    public void setActiva(Boolean activa) {this.activa = activa;}
+    public void setSaldo(double saldo) {this.saldo = saldo;}
+
+    //para mostrar en gui
+    @Override
+    public String toString() {
+        return tipo + " | Nº: " + numerodecuenta + " | Saldo: $" + saldo;
     }
 }
